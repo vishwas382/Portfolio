b2ce4204c83011d8004964fd5e9fed0c5c419de8
Bhangare Vishwas <vishwas.bhangare02@gmail.com>
2025-01-31 14:33:20 +0530

    Update views.py
    render path updated by updating template name from home.html to index.html

--- Portfolio/app/views.py
+++ Portfolio/app/views.py
@@ -7,4 +7,4 @@ def home(request):
     projects = Project.objects.all()
     documents = Documents.objects.all()
     resume = "https://drive.google.com/file/d/1RBfFxlTMQfQ7dU3gTHxLSgi0qSgClXsM/view?usp=drive_link"
-    return render(request, "home.html", {'projects':projects, 'documents':documents, 'resume':resume})
+    return render(request, "index.html", {'projects':projects, 'documents':documents, 'resume':resume})
